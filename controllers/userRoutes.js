dc67240fe2f544f17871a9c1f2710a6f71fd76c6
Chris <chris.feight@gmail.com>
2021-12-03 14:15:08 -0800

    database test

---
+++ controllers/userRoutes.js
@@ -0,0 +1,73 @@
+const express = require("express");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken")
+const router = express.Router();
+const tokenAuth = require("../middleware/tokenAuth")
+const { User,Post } = require("../models");
+
+router.post("/signup", (req, res) => {
+    User.create({
+      email: req.body.email,
+      password: req.body.password
+    })
+      .then(newUser => {
+        res.json(newUser);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json({ err });
+      });
+  });
+  
+router.post("/login", (req, res) => {
+    User.findOne({
+      where: {
+        email: req.body.email
+      }
+    })
+      .then(foundUser => {
+          if(!foundUser){
+              res.status(401).send("email or password is incorrect.")
+          }
+          else if(bcrypt.compareSync(req.body.password,foundUser.password)){
+              const token = jwt.sign({
+                email:foundUser.email,
+                id:foundUser.id
+              },
+              process.env.JWT_SECRET
+              ,{
+                expiresIn:"2h"
+              })    
+              res.json({
+                token:token,
+                user:foundUser
+              });
+          }
+          else {
+              res.status(401).send("email or password is incorrect")
+          }
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json({ err });
+      });
+  });
+  
+  router.get("/secretclub",tokenAuth, (req,res)=>{
+    res.send(`welcome to the club, ${req.user.email}`)
+  })
+  
+  router.get("/profile",tokenAuth, (req,res)=>{
+    User.findByPk(req.user.id).then(foundUser=>{
+      res.json(foundUser)
+    })
+  })
+  
+  router.get("/api/users/:id/posts",(req,res)=>{
+    User.findByPk(req.params.id,{include:[Post]}).then(foundUser=>{
+      res.json(foundUser)
+    })
+  })
+  
+  
+  module.exports = router;
