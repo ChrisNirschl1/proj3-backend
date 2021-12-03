056ee969669a6497a7e2a8b26c8b513fb3d5999b
Chris <chris.feight@gmail.com>
2021-12-03 14:45:53 -0800

    changing user route

---
+++ controllers/postRoutes.js
@@ -0,0 +1,104 @@
+const express = require("express");
+const router = express.Router();
+const { User, Post } = require("../models");
+const tokenAuth = require("../middleware/tokenAuth");
+const isMyPost = require("../middleware/isMyPost");
+
+//Get all posts
+router.get("/", (req, res) => {
+    Post.findAll()
+        .then(Posts => {
+            res.json(Posts);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({ err: err });
+        });
+});
+
+//Get all posts with user data
+router.get("/users", (req, res) => {
+    Post.findAll({
+        include: [User]
+    })
+        .then(posts => {
+            res.json(posts);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({ err: err });
+        });
+});
+
+//find one post
+router.get("/:id", (req, res) => {
+    Post.findByPk(req.params.id, { include: [User] })
+        .then(foundPost => {
+            if (foundPost) {
+                res.json(foundPost);
+            } else {
+                res.status(404).send("no such Post");
+            }
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({ err: err });
+        });
+});
+
+//create a post
+router.post("/", tokenAuth, (req, res) => {
+    Post.create({
+      songname: req.body.songname,
+      description: req.body.description,
+      UserId: req.user.id
+    })
+      .then(newPost => {
+        res.json(newPost);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json({ err: err });
+      });
+  });
+
+//edit post
+router.put("/:id", tokenAuth, isMyPost, (req, res) => {
+    Post.update(
+      {
+        songname: req.body.songname,
+        description: req.body.description,
+      },
+      {
+        where: {
+          id: req.params.id
+        }
+      }
+    )
+      .then(updatedPost => {
+        res.json(updatedPost);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json({ err: err });
+      });
+  });
+
+
+router.delete("/:id", tokenAuth, isMyPost, (req, res) => {
+    Post.destroy(
+        {
+          where: {
+            id: req.params.id
+          }
+        }
+      )
+        .then(delPost => {
+          res.json(delPost);
+        })
+        .catch(err => {
+          console.log(err);
+          res.status(500).json({ err: err });
+        });
+});
+module.exports = router;
